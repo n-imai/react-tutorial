2fd3e35e849b897fb6b9cb50221548cbf4b9ce56
Nobuo Imai <nobuo.imai@di-v.co.jp>
2020-05-23 08:49:39 +0900

    Add function which will happen by clicking the button

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class Square extends React.Component {
     return (
       <button 
         className="square" 
-        onClick={() => this.props.onClick()
+        onClick={() => this.props.onClick()}
       > 
         {this.props.value}
       </button>
@@ -23,6 +23,12 @@ class Board extends React.Component {
     };
   }
 
+  handleClick(i){
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+  
   renderSquare(i) {
     return (
       <Square 
